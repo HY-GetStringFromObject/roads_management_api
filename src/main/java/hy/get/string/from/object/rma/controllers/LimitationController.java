3bae0115fa7fc798faa6b7249586f155f4842a94
Mash <mbedra666@gmail.com>
2018-11-25 05:05:07 +0100

    Add cross origin to all controllers

--- src/main/java/hy/get/string/from/object/rma/controllers/LimitationController.java
+++ src/main/java/hy/get/string/from/object/rma/controllers/LimitationController.java
@@ -6,6 +6,7 @@ import net.bedra.maciej.mblogging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(value = "*", maxAge = 3600)
 public class LimitationController {
 
 	private static Logger log = Logger.getLogger();
--- src/main/java/hy/get/string/from/object/rma/controllers/SegmentLimitationController.java
+++ src/main/java/hy/get/string/from/object/rma/controllers/SegmentLimitationController.java
@@ -6,12 +6,14 @@ import net.bedra.maciej.mblogging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(value = "*", maxAge = 3600)
 public class SegmentLimitationController {
 
 	private static Logger log = Logger.getLogger();
